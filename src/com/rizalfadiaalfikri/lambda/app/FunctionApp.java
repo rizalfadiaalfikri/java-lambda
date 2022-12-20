4ce62d53721fb2444ec47a433b55d33272a933e2
Rizal Fadia Al Fikri <Rizalfadiaalfikri859@gmail.com>
2022-12-20 09:30:09 +0700

    predicate lambda

--- src/com/rizalfadiaalfikri/lambda/app/FunctionApp.java
+++ src/com/rizalfadiaalfikri/lambda/app/FunctionApp.java
@@ -4,15 +4,8 @@ import java.util.function.Function;
 
 public class FunctionApp {
     public static void main(String[] args) {
-        Function<String, Integer> functionLength = new Function<String, Integer>() {
 
-            @Override
-            public Integer apply(String value) {
-                // TODO Auto-generated method stub
-                return value.length();
-            }
-
-        };
+        Function<String, Integer> functionLength = value -> value.length();
 
         System.out.println(functionLength.apply("Rizal Fadia Al Fikri"));
     }
