329ff66628651c92b11ae457a8a4ef7721ad24f7
Rizal Fadia Al Fikri <Rizalfadiaalfikri859@gmail.com>
2022-12-21 07:51:55 +0700

    method reference non static

--- src/com/rizalfadiaalfikri/lambda/app/MethodReferenceApp.java
+++ src/com/rizalfadiaalfikri/lambda/app/MethodReferenceApp.java
@@ -6,9 +6,37 @@ import com.rizalfadiaalfikri.lambda.utils.StringUtils;
 
 public class MethodReferenceApp {
     public static void main(String[] args) {
-        Predicate<String> predicateIsLowerCase = t -> StringUtils.isLowerCase(t);
+        // Predicate<String> predicateIsLowerCase = t -> StringUtils.isLowerCase(t);
+        Predicate<String> predicateIsLowerCase = StringUtils::isLowerCase;
 
         System.out.println(predicateIsLowerCase.test("Rizal"));
         System.out.println(predicateIsLowerCase.test("rizal"));
+
+    }
+
+    public void run() {
+        // Predicate<String> predicateIsLowerCase = t -> StringUtils.isLowerCase(t);
+        Predicate<String> predicateIsLowerCase = this::isLowerCase;
+
+        System.out.println(predicateIsLowerCase.test("Rizal"));
+        System.out.println(predicateIsLowerCase.test("rizal"));
+    }
+
+    public void run2() {
+        // Predicate<String> predicateIsLowerCase = t -> StringUtils.isLowerCase(t);
+        MethodReferenceApp app = new MethodReferenceApp();
+        Predicate<String> predicateIsLowerCase = app::isLowerCase;
+
+        System.out.println(predicateIsLowerCase.test("Rizal"));
+        System.out.println(predicateIsLowerCase.test("rizal"));
+    }
+
+    public boolean isLowerCase(String value) {
+        for (var c : value.toCharArray()) {
+            if (!Character.isLowerCase(c)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
