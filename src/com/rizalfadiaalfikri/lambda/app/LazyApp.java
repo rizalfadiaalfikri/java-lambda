97a3b56ea8572ecd50a85b9151e68ecbe05baab2
Rizal Fadia Al Fikri <Rizalfadiaalfikri859@gmail.com>
2022-12-22 07:46:23 +0700

    lambda sebagai lazy parameter

---
+++ src/com/rizalfadiaalfikri/lambda/app/LazyApp.java
@@ -0,0 +1,22 @@
+package com.rizalfadiaalfikri.lambda.app;
+
+import java.util.function.Supplier;
+
+public class LazyApp {
+    public static void main(String[] args) {
+        testScore(70, () -> getName());
+    }
+
+    public static void testScore(int score, Supplier<String> name) {
+        if (score > 80) {
+            System.out.println("Selamat " + name.get() + ", Anda lulus");
+        } else {
+            System.out.println("Coba lagi tahun depan");
+        }
+    }
+
+    public static String getName() {
+        System.out.println("Method getName() dipanggil");
+        return "Rizal";
+    }
+}
